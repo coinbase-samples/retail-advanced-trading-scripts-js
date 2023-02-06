08e67321543fbfbb71501a6c33ae5c39e53d9f6e
Jay Parisi <jay.parisi@coinbase.com>
2023-02-06 12:24:10 -0500

    added api response log

--- ListAccounts.js
+++ ListAccounts.js
@@ -24,6 +24,7 @@ async function listAccounts() {
       },
     });
     const data = await response.json();
+    console.log(data);
   } catch (error) {
     console.log('this is the error', error);
   }
